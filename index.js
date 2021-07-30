113cce719f26b1bd41d0c37c38518efaf61e592b
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-07-30 05:55:37 +0530

    Moved generatePassword to a new file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const { program } = require('commander');
 
+const generatePassword = require('./generatePassword');
+
 program.version('1.0.0').description();
 program.description('Password Generator');
 
@@ -14,21 +16,3 @@ const { length } = program.opts();
 const generatedPassword = generatePassword(parseInt(length));
 
 console.log(generatedPassword);
-
-function generatePassword(length = 8) {
-    const charsLower = 'abcdefghijklmnopqrstuvwxyz';
-    const charsUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const symbols = '!@#$&*-=';
-
-    const charset = charsLower + charsUpper + numbers + symbols;
-
-    let password = '';
-
-    for (let i = 0; i < length; i++) {
-        const randomChar = charset[Math.floor(Math.random() * charset.length)];
-        password = password + randomChar;
-    }
-
-    return password;
-}
