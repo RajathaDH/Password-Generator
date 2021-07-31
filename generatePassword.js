16ccec9831a50235ef41f96ac8d2d92aaaca3acd
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-07-31 13:35:25 +0530

    Added arguments to enable numbers and symbols in password

--- generatePassword.js
+++ generatePassword.js
@@ -1,19 +1,33 @@
-const charsLower = 'abcdefghijklmnopqrstuvwxyz';
-const charsUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const numbers = '0123456789';
-const symbols = '!@#$&*-=';
+function generatePassword(length, opts) {
+    const charset = getCharset(opts);
 
-const charset = charsLower + charsUpper + numbers + symbols;
+    const password = createPassword(parseInt(length), charset);
 
-function generatePassword(length = 8) {
+    return password;
+}
+
+function createPassword(length = 8, charset) {
     let password = '';
 
     for (let i = 0; i < length; i++) {
+        // get random character from charset and append to password
         const randomChar = charset[Math.floor(Math.random() * charset.length)];
         password = password + randomChar;
     }
 
     return password;
+}
+
+function getCharset(opts = { numbers: false, symbols: false }) {
+    const charsLower = 'abcdefghijklmnopqrstuvwxyz';
+    const charsUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // add numbers and symbols to charset if options passed in as arguments
+    const numbers = opts.numbers ? '0123456789' : '';
+    const symbols = opts.symbols ? '!@#$&*-=' : '';
+
+    const charset = charsLower + charsUpper + numbers + symbols;
+
+    return charset;
 }
 
 module.exports = generatePassword;
