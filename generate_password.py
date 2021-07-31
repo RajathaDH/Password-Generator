a59736fafe81371d9eab7b8bd22719c7fbc2c7f0
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-07-31 14:22:40 +0530

    Updated generate password with command line arguments

--- generate_password.py
+++ generate_password.py
@@ -1,22 +1,47 @@
 import argparse
+import random
 
 def get_charset(opts):
-    print(opts)
+    charsLower = 'abcdefghijklmnopqrstuvwxyz'
+    charsUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # add numbers and symbols to charset if options passed in as arguments
+    numbers = '0123456789' if opts.get('allow_numbers', False) else ''
+    symbols = '!@#$&*-=' if opts.get('allow_symbols', False) else ''
 
-    return ''
+    charset = charsLower + charsUpper + numbers + symbols
+
+    return charset
+
 
 def generate_password(length = 8, charset = ''):
     password = ''
 
     for _ in range(length):
-        password = password + ''
+        # get random character from charset and append to password
+        password = password + charset[random.randrange(len(charset))]
 
     return password
 
-def main():
-    charset = get_charset({})
 
-    password = generate_password(10, charset)
+def main():
+    parser = argparse.ArgumentParser(description='generate a random password')
+
+    parser.add_argument('-l', '--length', type=int, required=False, default=8)
+    parser.add_argument('-n', '--numbers', action='store_true', help='allow numbers')
+    parser.add_argument('-s', '--symbols', action='store_true', help='allow symbols')
+
+    args = parser.parse_args()
+
+    length = args.length
+    opts = {
+        'allow_numbers': args.numbers,
+        'allow_symbols': args.symbols
+    }
+
+    charset = get_charset(opts)
+    print(opts)
+
+    password = generate_password(int(length), charset)
 
     print(password)
 
